2febb0fe1c90c42a9d845a86cbf2cb4ac113fc63
jun951231 <jun951231@gmail.com>
2021-08-17 10:31:39 +0900

    Aug 17

---
+++ src/test/java/com/example/junn/demodocker2/book/BookApplicationTest.java
@@ -0,0 +1,15 @@
+package com.example.junn.demodocker2.book;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class BookApplicationTest {
+
+    @Test
+    void contextLoads(){
+
+    }
+}
